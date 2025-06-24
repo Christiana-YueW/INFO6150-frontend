a162a1497d1c85288770d8aa662a4454c7ac84ef
Yue Wu <wu.y3@northeastern.edu>
2025-06-23 22:36:41 -0400

    these changes apply to the js draft

--- work/07-js-form/public/scripts.js
+++ work/07-js-form/public/scripts.js
@@ -24,7 +24,7 @@
     })
 
     confirmInput.addEventListener("input", (e) => {
-        if (confirmInput.value !== emailInput.value) {
+        if (confirmInput.value && confirmInput.value !== emailInput.value) {
             confirmError.innerText = "Emails do not match";
         } else {
             confirmError.innerText = "";
