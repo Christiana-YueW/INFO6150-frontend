5a734d4bfbe3f243507c9af61ef1c96845755020
Yue Wu <wu.y3@northeastern.edu>
2025-07-14 17:39:04 -0400

    add data draft files

---
+++ work/10-react-components/components/src/pages/TextPage.jsx
@@ -0,0 +1,13 @@
+function TextPage() {
+
+    return (
+        <section>
+            <h2> Welcome to Purely Lemon </h2>
+            <p> A tribute to the world’s most vibrant fruit — from kitchen to skincare, from zesty recipes to citrus inspiration, </p>
+            <p> At Purely Lemon, we celebrate the small, sunny fruit that brings a bold burst of flavor, fragrance, and vitality to life. Lemons are more than just a garnish — they’re a lifestyle. With their refreshing aroma, immune-boosting properties, and endless versatility, lemons are nature’s little powerhouse. </p>
+            <p> You You can explore Cards or Panels using the navigation above. </p>
+        </section>
+    )
+}
+
+export default TextPage;
